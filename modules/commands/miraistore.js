33817a3a54f3a2007862b48dfc83cfe80e31806c
rX Abdullah <167834738+rxabdullah007@users.noreply.github.com>
2025-12-26 19:56:49 +0600

    Update miraistore.js

--- modules/commands/miraistore.js
+++ modules/commands/miraistore.js
@@ -1,6 +1,6 @@
 const axios = require("axios");
 
-const API_BASE = "https://mirai-store.onrender.com/";
+const API_BASE = "https://mirai-store.onrender.com";
 const ADMINS = [
   "100068565380737"
 ];
